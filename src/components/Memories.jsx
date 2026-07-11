49fa4fd494deecef29294930d33aee4dd9a06b86
ANA3501 <pv27649@alunos.estgv.ipv.pt>
2026-07-11 19:40:20 +0100

    dudi

--- src/components/Memories.jsx
+++ src/components/Memories.jsx
@@ -6,7 +6,10 @@ import Rosa from '../assets/Rosa.jpg'
 import JantarCurso from '../assets/JantarCurso.jpg'
 import Algarve2 from '../assets/Algarve2.jpg'
 import Algarve2_1 from '../assets/Algarve2_1.jpg'
+import Algarve2_2 from '../assets/Algarve2_2.jpg'
+import Algarve2_3 from '../assets/Algarve2_3.jpg'
 import Florzinha from '../assets/Florzinha.jpg'
+import PrimeiraRosa from '../assets/PrimeiraRosa.jpg'
 
 // Cada memória pode ter várias fotos em "images: [...]"
 // Substitui os "images: []" pelas tuas fotos reais, ex:
@@ -21,10 +24,10 @@ export const memories = [
   { id: 3, images: [Rosa], 
     caption: "Este dia foi a primeira rosaq ue te ofereci, comprei uma branca porque dissesteq ue são as que mais gostas.", 
     date: "6 de fevereiro 2025" },
-  { id: 4, images: [JantarCurso], caption: "esse dia foi o mais aconchegante", date: "maio, 2024" },
-  { id: 5, images: [Florzinha], caption: "esse dia foi o que eu não vou esquecer", date: "maio, 2024" },
-  { id: 6, images: [Algarve2_1, Algarve2], caption: "esse dia foi o que floresceu em mim", date: "junho, 2024" },
-  { id: 7, images: [], caption: "esse dia foi o que me inspirou", date: "junho, 2024" },
+  { id: 4, images: [JantarCurso], caption: "Este foi o dia em que os nossos professores ficaram felizes por estarmos juntos.", date: "6 fevereiro, 2026" },
+  { id: 5, images: [Florzinha], caption: "Esta foi a primeira vez que apanhaste uma florzinha da rua para mim, parece tão pouco mas fiquei tão feliz queria chorar.", date: "4 de março, 2025" },
+  { id: 6, images: [Algarve2_2, Algarve2_1, Algarve2,Algarve2_3], caption: "Esta foi a minha segunda ida ao Algarve para te ver meu amor.", date: "13 Agosto, 2025" },
+  { id: 7, images: [PrimeiraRosa], caption: "Esta foi a primeira flor que me deste, fui para casa no fim de semana super contente, se não me enagano fartei me de te pedir ao sair do nb,", date: "7 de fevereiro, 2025" },
   { id: 8, images: [], caption: "esse dia foi o que começou devagar", date: "julho, 2024" },
   { id: 9, images: [], caption: "esse dia foi o que mudou tudo", date: "julho, 2024" },
   { id: 10, images: [], caption: "esse dia foi o que eu chorei de rir", date: "agosto, 2024" },
